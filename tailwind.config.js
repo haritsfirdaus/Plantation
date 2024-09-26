9f72be1325fb3175a857b4137f6924abcd78fea6
haritsfirdaus <haritsfird@gmail.com>
2024-09-26 23:51:09 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./home.html'],
+  theme: {
+    extend: {
+      colors : {
+        "hijau" : "#0F2615",
+        "cream" : "#D9D389",
+        "ijoijo" : "#465928",
+        "coklat" : "#D9D389"
+      },
+    },
+  },
+  plugins: [],
+}
+
